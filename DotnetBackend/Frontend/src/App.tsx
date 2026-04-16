6717c346ca0093c2c8bb0d42d39ac47240bf11d1
Edris9 <Edriskohestani1010@gmail.com>
2026-04-16 10:39:18 +0200

    created frontend

---
+++ DotnetBackend/Frontend/src/App.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import { AuthProvider } from "./context/AuthContext"
+import ProtectedRoute from "./components/ProtectedRoute"
+import LoginPage from "./pages/LoginPage"
+import HomePage from "./pages/HomePage"
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <AuthProvider>
+                <Routes>
+                    <Route path="/" element={<Navigate to="/login" replace />} />
+                    <Route path="/login" element={<LoginPage />} />
+                    <Route path="/home" element={
+                        <ProtectedRoute>
+                            <HomePage />
+                        </ProtectedRoute>
+                    } />
+                </Routes>
+            </AuthProvider>
+        </BrowserRouter>
+    )
+}
